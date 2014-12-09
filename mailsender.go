0362c96882cbc9409e6f97c79a4fa770a872520e
Zlodimir <sslupinos@gmail.com>
2014-12-09 12:57:51 +0300

    return string and error if occured

--- mailsender.go
+++ mailsender.go
@@ -3,8 +3,8 @@ package mailsender
 import (
         "log"
         "net/smtp"
-        "fmt"
         "strconv"
+	"errors"
 )
 
 type EmailUser struct {
@@ -22,27 +22,23 @@ type SmtpTemplateData struct {
 }
 
 
-func Sendmail(from string, to []string, subject string, body string) string {
+func Sendmail(from string, to []string, subject string, body string) (string, error) {
 
 	emailUser := &EmailUser{"ZlodimirBot@gmail.com", "ZlodimirBotPassword", "smtp.gmail.com", 587}
-	fmt.Println("Preparing auth object...")
         auth := smtp.PlainAuth("", emailUser.Username, emailUser.Password, emailUser.EmailServer)
 	
 	mime := "MIME-version: 1.0;\nContent-Type: text/html; charset=\"UTF-8\";\n\n";
 	subj := "Subject: " +  subject + "\n"
 	msg := []byte(subj + mime + "<html><body><h3>" + body + "</h3></body></html>")
+
         // and send the email all in one step.
-        err := smtp.SendMail(emailUser.EmailServer + ":" + strconv.Itoa(emailUser.Port), auth, from,
-                to,
-               	msg)
+        err := smtp.SendMail(emailUser.EmailServer + ":" + strconv.Itoa(emailUser.Port), auth, from, to, msg)
         if err != nil {
                 log.Fatal(err)
-		fmt.Println("Error sending message...")
-	} else {
-		fmt.Println("Message sent...")
-        }
-	
-	return "Message sent ..."
+		return "", errors.New("Error while sending email...")
+	}
+
+	return "Message sent ...", nil
 }
 
 
